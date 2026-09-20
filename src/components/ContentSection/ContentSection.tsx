25b6ca60256daca9d62963cd8d3dcf6904f0c066
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-09-20 19:31:44 +0200

    Add generic ContentSection component

---
+++ src/components/ContentSection/ContentSection.tsx
@@ -0,0 +1,34 @@
+import styles from "./ContentSection.module.css";
+
+interface ContentSectionProps {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+function ContentSection({ children, style }: ContentSectionProps) {
+  return (
+    <section className={styles.section} style={style}>
+      <div className={styles.content}>{children}</div>
+    </section>
+  );
+}
+
+interface ContentSectionTitleProps {
+  children: React.ReactNode;
+}
+
+function ContentSectionTitle({ children }: ContentSectionTitleProps) {
+  return <h2 className={styles.title}>{children}</h2>;
+}
+
+interface ContentSectionSubTitleProps {
+  children: React.ReactNode;
+}
+
+function ContentSectionSubTitle({ children }: ContentSectionSubTitleProps) {
+  return <p className={styles["sub-title"]}>{children}</p>;
+}
+
+ContentSection.Title = ContentSectionTitle;
+ContentSection.SubTitle = ContentSectionSubTitle;
+export default ContentSection;
