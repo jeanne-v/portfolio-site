e56e7fa356a4733926f5d2f67ef043922375ce01
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-09-20 19:32:00 +0200

    Add LinksSection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LinksSection from "./components/LinksSection/LinksSection";
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
@@ -5,6 +6,9 @@ import "./App.css";
 export default function App() {
   return (
     <div>
+      <main>
+        <LinksSection />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/components/LinksSection/LinksSection.tsx
@@ -0,0 +1,30 @@
+import ContentSection from "../ContentSection/ContentSection";
+import ButtonLink from "../ButtonLink/ButtonLink";
+
+import linkedInIcon from "../../assets/linkedin-icon-white.svg";
+import gitHubIcon from "../../assets/github-icon-purple.svg";
+
+import styles from "./LinksSection.module.css";
+
+export default function LinksSection() {
+  return (
+    <ContentSection style={{ backgroundColor: "var(--secondary-bg-color)" }}>
+      <ContentSection.Title>Links</ContentSection.Title>
+      <ContentSection.SubTitle>Places where you can find me:</ContentSection.SubTitle>
+      <div className={styles.links}>
+        <ButtonLink
+          text="LinkedIn"
+          url="https://www.linkedin.com/in/jeanne-viallard-8090b5251/"
+          variant="primary"
+          icon={linkedInIcon}
+        />
+        <ButtonLink
+          text="Github"
+          url="https://github.com/jeanne-v"
+          variant="secondary"
+          icon={gitHubIcon}
+        />
+      </div>
+    </ContentSection>
+  );
+}
