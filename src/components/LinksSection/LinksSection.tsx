cb2911166483e5653e0b8d258193de708a99a566
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-09-21 16:20:33 +0200

    Add scroll transition to projects section and links section

--- src/components/LinksSection/LinksSection.tsx
+++ src/components/LinksSection/LinksSection.tsx
@@ -1,5 +1,6 @@
 import ContentSection from "../ContentSection/ContentSection";
 import ButtonLink from "../ButtonLink/ButtonLink";
+import useScrollStatus from "../../hooks/useScrollStatus";
 
 import linkedInIcon from "../../assets/linkedin-icon-white.svg";
 import gitHubIcon from "../../assets/github-icon-purple.svg";
@@ -7,11 +8,15 @@ import gitHubIcon from "../../assets/github-icon-purple.svg";
 import styles from "./LinksSection.module.css";
 
 export default function LinksSection() {
+  const { hasBeenScrolledIntoView, elementRef } = useScrollStatus<HTMLDivElement>();
   return (
     <ContentSection style={{ backgroundColor: "var(--secondary-bg-color)" }} id="links">
       <ContentSection.Title>Links</ContentSection.Title>
       <ContentSection.SubTitle>Places where you can find me:</ContentSection.SubTitle>
-      <div className={styles.links}>
+      <div
+        ref={elementRef}
+        className={`${styles.links} ${hasBeenScrolledIntoView ? styles.active : ""}`}
+      >
         <ButtonLink
           text="LinkedIn"
           url="https://www.linkedin.com/in/jeanne-viallard-8090b5251/"
--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,17 +1,22 @@
 import ContentSection from "../ContentSection/ContentSection";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import projects from "../../data/projects";
+import useScrollStatus from "../../hooks/useScrollStatus";
 
 import styles from "./ProjectsSection.module.css";
 
 export default function ProjectsSection() {
+  const { hasBeenScrolledIntoView, elementRef } = useScrollStatus<HTMLDivElement>();
   return (
     <ContentSection id="projects">
       <ContentSection.Title>Projects</ContentSection.Title>
       <ContentSection.SubTitle>
         Here are some of my latest projects:
       </ContentSection.SubTitle>
-      <div className={styles.projects}>
+      <div
+        ref={elementRef}
+        className={`${styles.projects} ${hasBeenScrolledIntoView ? styles.active : ""}`}
+      >
         {projects.map((project, index) => {
           return (
             <ProjectCard
